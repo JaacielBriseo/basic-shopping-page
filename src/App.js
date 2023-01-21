78f121fdd2a73d5615fe14e9171b42453fd54674
JaacielBriseo <jaacielbris23@gmail.com>
2023-01-20 23:08:36 -0700

    Products context added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { Home, Navigation } from './routes';
 import { Route, Routes } from 'react-router-dom';
 import { Authentication } from './routes/authentication/authentication.component';
+import { Shop } from './routes/shop/shop.component';
 
-const Shop = () => {
-	return <h1>I am The Shop page</h1>;
-};
+
 const App = () => (
 	<Routes>
 		<Route path='/' element={<Navigation />}>
